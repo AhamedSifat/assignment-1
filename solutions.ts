cc69d587e9ffa3fb821678c6568d8fcd31c51e57
sifat1234 <ahahmedsifat111@gmail.com>
2026-05-07 22:27:03 +0600

    refactor: remove console.logs and clean up formatting in solutions.ts

--- solutions.ts
+++ solutions.ts
@@ -1,41 +1,32 @@
-
-//problem 1
-const arr: number[]= [1, 2, 3, 4, 5, 6];
+const arr: number[] = [1, 2, 3, 4, 5, 6];
 
 const filterEvenNumbers = (numbers: number[]): number[] => {
-    return numbers.filter(num => num % 2 === 0);
-}
+  return numbers.filter((num) => num % 2 === 0);
+};
 
 const evenNumbers = filterEvenNumbers(arr);
 
-//problem 2
+const value: string = "typescript";
 
-const value: string='typescript';
- const reverseString = (str: string): string=>  {
-    return str.split('').reverse().join('');
- }
- console.log(reverseString(value));
+const reverseString = (str: string): string => {
+  return str.split("").reverse().join("");
+};
 
- //problem 3
+const stringOrNumber: string | number = "Hello";
 
- const stringOrNumber: string | number = 'Hello';
+const checkType = (input: string | number): string => {
+  if (typeof input === "string") {
+    return "string";
+  }
+  return "number";
+};
 
- const checkType = (input: string | number): string => {
-    if (typeof input === 'string') {
-        return 'string';
-    }
-    return 'number';
-    }
-console.log(checkType(stringOrNumber));
-
-//problem 4
 const user = { id: 1, name: "John Doe", age: 21 };
-const getProperty= <T, K extends keyof T> (obj: T, key: K) :T[K] => {
-    return obj[key];
-}
-console.log(getProperty(user, 'name'));
 
-//problem 5
+const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => {
+  return obj[key];
+};
+
 interface Book {
   title: string;
   author: string;
@@ -43,49 +34,40 @@ interface Book {
 }
 
 interface bookwithIsRead extends Book {
-    isRead: boolean;
+  isRead: boolean;
 }
 
-const toggleReadStatus = (book: Book, isRead: boolean= true): bookwithIsRead => {
-    return { ...book, isRead };
-
-}
+const toggleReadStatus = (book: Book, isRead: boolean = true): bookwithIsRead => {
+  return { ...book, isRead };
+};
 
 const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
-console.log(toggleReadStatus(myBook));
 
-//problem 6
 class Person {
-    name: string;
-    age: number;
-    grade: string;
+  name: string;
+  age: number;
+  grade: string;
 
-    constructor(name: string, age: number, grade: string) {
-        this.name = name;
-        this.age = age;
-        this.grade = grade;
-    }
+  constructor(name: string, age: number, grade: string) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
 }
 
 class Student extends Person {
-    constructor(name: string, age: number, grade: string) {
-        super(name, age, grade);
-    }
-    getDetails(): string {
-        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
-    }
+  constructor(name: string, age: number, grade: string) {
+    super(name, age, grade);
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
 }
 
 const student = new Student("Alice", 20, "A");
-console.log(student.getDetails());
 
-
-//problem 7
-
-const getIntersection =(arr1: number[], arr2: number[]): number[] => {
-const set = new Set(arr2);
+const getIntersection = (arr1: number[], arr2: number[]): number[] => {
+  const set = new Set(arr2);
   return arr1.filter((num) => set.has(num));
-}
-console.log(getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7])
-)
-
+};
